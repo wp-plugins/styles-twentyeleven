e2967fe1c9cedf15bd9def9b1130068c1dc60803
pdclark <pdclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-21 01:55:52 +0000

    Tagging version 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/styles-twentyeleven/tags/1.0.3@729150 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Styles: TwentyEleven
 Plugin URI: http://stylesplugin.com
 Description: Add Customize options to the <a href="http://wordpress.org/extend/themes/twentyeleven" target="_blank">TwentyEleven theme</a> using the <a href="http://wordpress.org/extend/plugins/styles/" target="_blank">Styles plugin</a>.
-Version: 1.0.2
+Version: 1.0.3
 Author: Brainstorm Media
 Author URI: http://brainstormmedia.com
 
